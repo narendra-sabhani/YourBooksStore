f9e53b6e322a0f7b9e1786a57b7b0b3bbaa62f5f
Narendra <narendrasab1993@outlook.com>
2020-09-14 00:13:06 +0530

    Implemented search functionality on Books/List page

--- YourBooksStore/Pages/Books/List.cshtml.cs
+++ YourBooksStore/Pages/Books/List.cshtml.cs
@@ -12,15 +12,18 @@ namespace YourBooksStore.Pages.Books {
     public class ListModel : PageModel {
         IConfiguration Configuration { get; }
         IBookData BookData { get; }
-
         public IEnumerable<Book> Books { get; set; }
+
+       [BindProperty(SupportsGet=true)]
+       public string SearchTerm { get; set; }
         public ListModel (IConfiguration configuration, IBookData bookData) {
             this.BookData = bookData;
             this.Configuration = configuration;
 
         }
         public void OnGet () {
-            Books = BookData.GetAll ();
+           
+            Books = BookData.GetAllBooksByName(SearchTerm);
         }
     }
 }
